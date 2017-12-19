77b14ff7c3fefb5297bc1c16c0091fc353ba53b7
Jess <maochengjie1988@gmail.com>
2017-12-19 18:13:52 +0800

    增加入口处暴露函数

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
  * 
  */
 
+'use strict';
+
+const readDir = require('./lib/read_dir.js');
+const diffDir = require('./lib/diff_dir.js');
 
 module.exports = {
-
+    read: readDir,
+    diff: diffDir
 };
