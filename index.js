967b31440cb74f3409877afc41c2df6014e1279d
Jess <maochengjie1988@gmail.com>
2018-01-08 19:18:35 +0800

    export md5 sign

--- index.js
+++ index.js
@@ -6,8 +6,10 @@
 
 const readDir = require('./lib/read_dir.js');
 const diffDir = require('./lib/diff_dir.js');
+const sign = require('./lib/sign.js');
 
 module.exports = {
     read: readDir,
-    diff: diffDir
+    diff: diffDir,
+    sign : sign
 };
